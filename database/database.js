b7bbc3e18d497c7881bcae33e82a2825d8e885ec
lucasbatista2408 <lucasbatista2408@gmail.com>
2022-08-07 08:09:32 -0300

    heroku test

--- database/database.js
+++ database/database.js
@@ -5,30 +5,17 @@ dotenv.config();
 
 const { Pool } = pg;
 
+const db_uri = process.env.DATABASE_URI
+console.log(db_uri)
+
 const databaseConfig = {
-  connectionString: process.env.DATABASE_URI,
+  connectionString: db_uri,
   ssl: {
       rejectUnauthorized: false
   }
 }
 
 const client = new Pool (databaseConfig);
-
-
-
-// const user = process.env.USER;
-// const password = process.env.PASSWORD;
-// const hostname = process.env.HOST;
-// const port = process.env.PORT_DB;
-// const database = process.env.DATABASE;
-
-// const client = new Pool({
-//   user,
-//   password,
-//   hostname,
-//   port,
-//   database
-// });
 
 export default client;
 
