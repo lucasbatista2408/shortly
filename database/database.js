87751e1b349f8060e9b1996d3155ed13fe5afaa4
lucasbatista2408 <lucasbatista2408@gmail.com>
2022-08-07 07:31:52 -0300

    prepare for deploy

--- database/database.js
+++ database/database.js
@@ -3,23 +3,30 @@ import dotenv from "dotenv"
 
 dotenv.config();
 
+const databaseConfig = {
+  connectionString: process.env.DATABASE_URI,
+  ssl: {
+      rejectUnauthorized: false
+  }
+}
 
+const client = new Pool(databaseConfig);
 
-const { Pool } = pg;
+// const { Pool } = pg;
 
-const user = process.env.USER;
-const password = process.env.PASSWORD;
-const hostname = process.env.HOST;
-const port = process.env.PORT_DB;
-const database = process.env.DATABASE;
+// const user = process.env.USER;
+// const password = process.env.PASSWORD;
+// const hostname = process.env.HOST;
+// const port = process.env.PORT_DB;
+// const database = process.env.DATABASE;
 
-const client = new Pool({
-  user,
-  password,
-  hostname,
-  port,
-  database
-});
+// const client = new Pool({
+//   user,
+//   password,
+//   hostname,
+//   port,
+//   database
+// });
 
 export default client;
 
